629ef6c620ff3b804b477d488e98d620a60ce34e
Vinay Gavirangaswamy (vgaviran) <vgaviran@cisco.com>
2020-02-17 11:10:59 -0800

    A clean new branch for containerized RDMTk

--- rdmtk_r_scripts/RDMTkAnalysisR/R/scalepars.R
+++
@@ -1,2 +0,0 @@
-scalepars=function(tpars,lb,ub)  				#scales pars for bounded optim
-  -log(((ub-lb)/(tpars-lb))-1)					#lb=lower bound, ub=upper bound
--- rdmtk_r_scripts/RDMTkAnalysisR/R/unscalepars.R
+++
@@ -1,2 +0,0 @@
-unscalepars=function(tpars,lb,ub)				#unscales pars
-  (ub-lb)/(1+exp(-tpars))+lb					#lb=lower bound, ub=upper bound
